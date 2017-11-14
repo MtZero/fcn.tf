face81e32546630f3faa057bcef36c19cb021ffc
Chen Guanqi <30858327+ChenGQ26@users.noreply.github.com>
2017-11-14 23:38:19 +0800

    add score layer to pool3 and pool4

--- fcn_8s.py
+++ fcn_8s.py
@@ -77,14 +77,20 @@ def inference(image, keep_prob):
         W_t1 = tf.Variable(initial_value=tf.truncated_normal([4, 4, deconv_shape1[3].value, 21], 0.02), name="W_t1")
         b_t1 = tf.Variable(initial_value=tf.constant(0.0, shape=[deconv_shape1[3].value]), name="b_t1")
         conv_t1 = vgg16_object.conv2d_transpose_strided(fc8, W_t1, b_t1, output_shape=tf.shape(pool4))
-        fuse_1 = tf.add(conv_t1, pool4, name="fuse_1")
+        score1_W = utils.weight_variable([1,1, 512, 21], name="score1_W")
+        score1_b = utils.bias_variable([21], name="score1_b")
+        score1 = vgg16_object._conv(pool4, score1_W, score1_b, 'score1')
+        fuse_1 = tf.add(conv_t1, score1, name="fuse_1")
 
         # upsample and add with pool5
         deconv_shape2 = pool3.get_shape()
         W_t2 = tf.Variable(initial_value=tf.truncated_normal([4, 4, deconv_shape2[3].value, deconv_shape1[3].value], 0.02), name="W_t2")
         b_t2 = tf.Variable(initial_value=tf.constant(0.0, shape=[deconv_shape2[3].value]), name="b_t2")
         conv_t2 = vgg16_object.conv2d_transpose_strided(fuse_1, W_t2, b_t2, output_shape=tf.shape(pool3))
-        fuse_2 = tf.add(conv_t2, pool3, name="fuse_2")
+        score2_W = utils.weight_variable([1,1, 512, 21], name="score2_W")
+        score2_b = utils.bias_variable([21], name="score2_b")
+        score2 = vgg16_object._conv(pool3, score2_W, score2_b, 'score2')
+        fuse_2 = tf.add(conv_t2, score2, name="fuse_2")
 
         shape = tf.shape(processed_image)
         deconv_shape3 = tf.stack([shape[0], shape[1], shape[2], 21])
@@ -101,7 +107,6 @@ def train(loss_val, var_list):
     optimizer = tf.train.MomentumOptimizer(FLAGS.learning_rate, FLAGS.momentum)
     grads = optimizer.compute_gradients(loss_val, var_list=var_list)
     if FLAGS.debug:
-        # print(len(var_list))
         for grad, var in grads:
             utils.add_gradient_summary(grad, var)
     return optimizer.apply_gradients(grads)
