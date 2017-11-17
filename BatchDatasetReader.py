9d25de7daf46fec49ff0016158f56cf6ee83337f
YungLin <jingyanglin96@gmail.com>
2017-11-17 16:54:21 +0800

    update

--- BatchDatasetReader.py
+++ BatchDatasetReader.py
@@ -1,9 +1,11 @@
 """
 Code ideas from https://github.com/Newmu/dcgan and tensorflow mnist dataset reader
 """
+import tensorflow as tf
 import numpy as np
 import scipy.misc as misc
 from PIL import Image
+import os, sys
 
 
 class BatchDatasetReader:
@@ -14,7 +16,7 @@ class BatchDatasetReader:
     batch_offset = 0
     epochs_completed = 0
 
-    def __init__(self, records_list, image_options={}):
+    def __init__(self, records_list, image_options={}, data_dir=""):
         """
         Intialize a generic file reader with batching for list of files
         :param records_list: list of file records to read -
@@ -28,19 +30,27 @@ class BatchDatasetReader:
         print("Initializing Batch Dataset Reader...")
         print(image_options)
         self.files = records_list
+        self.data_dir = data_dir
         self.image_options = image_options
+        self.images_filename = "JPEGImages_tranformed"
+        self.annotations_filename = "SegmentationClass_tranformed"
         self._read_images()
+        
 
     def _read_images(self):
         self.__channels = True
-        self.images = np.array([self._transform(filename) for filename in self.files["images"]])
+        images_filepath = os.path.join(self.data_dir, self.images_filename)
+        annotations_filepath = os.path.join(self.data_dir, self.annotations_filename)
+        self.preprocess(images_filepath, "images")
         self.__channels = False
-        self.annotations = np.array([self._transform(filename) for filename in self.files["annotations"]])
+        self.preprocess(annotations_filepath, "annotations")
         print(self.images.shape)
         print(self.annotations.shape)
 
     def _transform(self, filename):
         image = np.array(Image.open(filename))
+        img_h, img_w = image.shape[0], image.shape[1]
+
         # ensure that the pictures and the annotations have the same dimensions
         if self.__channels and len(image.shape) < 3:  # make sure images are of shape(h,w,3)
             image = np.array([image for i in range(3)])
@@ -48,15 +58,16 @@ class BatchDatasetReader:
             image = image.transpose([1, 2, 0])
 
         # fill the image to 500x500
-        img_fill = np.zeros([500, 500, 3], 'uint8')
-        img_h, img_w = image.shape[0], image.shape[1]
-        img_h_fill = (500 - img_h) // 2
-        img_w_fill = (500 - img_w) // 2
-        for idx_i, i in enumerate(image):
-            for idx_j, j in enumerate(i):
-                img_fill[idx_i + img_h_fill][idx_j + img_w_fill] = j
-        image = img_fill
-        del img_fill
+        if img_h < 500 or img_w < 500:
+            img_fill = np.zeros([500, 500, 3], 'uint8') if len(image.shape) == 3 else np.zeros([500, 500], 'uint8')
+            
+            img_h_fill = (500 - img_h) // 2
+            img_w_fill = (500 - img_w) // 2
+            for idx_i, i in enumerate(image):
+                for idx_j, j in enumerate(i):
+                    img_fill[idx_i + img_h_fill][idx_j + img_w_fill] = j
+            image = img_fill
+            del img_fill
 
         if self.image_options.get("resize", False) and self.image_options["resize"]:
             resize_size = int(self.image_options["resize_size"])
@@ -66,6 +77,35 @@ class BatchDatasetReader:
             resize_image = image
 
         return np.array(resize_image)
+
+    def preprocess(self, filepath, filetype):
+        if not os.path.exists(filepath):
+            os.makedirs(filepath)
+            if filetype == "images":
+                self.images = np.array([self._transform(filename) for filename in self.files[filetype]])
+            else:
+                self.annotations = np.array([self._transform(filename) for filename in self.files[filetype]])
+            self.save_records(filetype)
+        else:
+            origin = "JPEGImages" if filetype == "images" else "SegmentationClass"
+            transformed = self.images_filename if filetype == "images" else self.annotations_filename
+            if filetype == "images":
+                self.images = np.array([np.array(Image.open(filename.replace(origin, transformed))) for filename in self.files[filetype]])
+            else:
+                self.annotations = np.array([np.array(Image.open(filename.replace(origin, transformed))) for filename in self.files[filetype]])
+            
+    def save_records(self, filetype):
+        i = 0
+        origin = "JPEGImages" if filetype == "images" else "SegmentationClass"
+        transformed = self.images_filename if filetype == "images" else self.annotations_filename
+        for filename in self.files[filetype]:
+            if filetype == "images":
+                im = Image.fromarray(self.images[i])
+                im.save(filename.replace(origin, transformed))
+            else:
+                im = Image.fromarray(self.annotations[i])
+                im.save(filename.replace(origin, transformed))
+            i += 1
 
     def get_records(self):
         return self.images, self.annotations
